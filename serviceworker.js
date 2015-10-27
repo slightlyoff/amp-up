ed6fb731cfeb52a5daf32134e1eee1244705b9c4
Alex Russell <slightlyoff@chromium.org>
2015-10-27 11:26:19 +0900

    Re-adding inlined CSS.

--- serviceworker.js
+++ serviceworker.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var VERSION = 2;
+var VERSION = 4;
 
 importScripts("src/serviceworker-cache-polyfill.js");
 
@@ -12,7 +12,7 @@ var dir = l.pathname.substring(0, l.pathname.lastIndexOf("serviceworker.js"));
 var root = l.origin + dir;
 
 var CORE_CACHE = VERSION + "-amp-up";
-var ITEM_CACHE = VERSION + "-amp-up-items";
+var ITEM_CACHE = VERSION + "-amp-up-article-cache";
 var START_AMP_CONTENT = "<!--___START_AMP_CONTENT___-->";
 var END_AMP_CONTENT = "<!--___END_AMP_CONTENT___-->";
 
@@ -23,6 +23,7 @@ self.addEventListener("install", function(e) {
       return c.addAll([
         root + "src/shell.html",
         root + "src/styles.css",
+        root + "src/webcomponents-lite.min.js",
         root + "src/elements.v.html",
         root + "src/elements.v.js",
         new Request("https://cdn.ampproject.org/v0/amp-audio-0.1.js", { mode: "no-cors" }),
@@ -37,6 +38,7 @@ self.addEventListener("install", function(e) {
         new Request("https://cdn.ampproject.org/v0.js", { mode: "no-cors" }),
         new Request("https://fonts.googleapis.com/css?family=Roboto:400,300,300italic,400italic,500,500italic,700,700italic", { mode: "no-cors" }),
         new Request("https://fonts.googleapis.com/css?family=Roboto+Mono:400,700", { mode: "no-cors" }),
+        new Request("https://cdn.rawgit.com/webcomponents/webcomponentsjs/v0.7.15/webcomponents-lite.min.js", { mode: "no-cors"}),
       ]).then(
         function() {
           return self.skipWaiting();
@@ -62,29 +64,74 @@ self.addEventListener("activate", function(e) {
   );
 });
 
-var lastBody = "";
+var AMPOnly = function(text) {
+  var end = text.lastIndexOf(END_AMP_CONTENT);
+  text = text.substring(0, end);
+  var start = text.indexOf(START_AMP_CONTENT);
+  text = text.substring(start + (START_AMP_CONTENT).length);
+  return text;
+}
+
+var postToMatchingWindow = function(text, url) {
+  return clients.matchAll({ type: "window" }).then(function(clients) {
+    clients.forEach(function(c) {
+      log(c, url.indexOf(c.url));
+      if (url.indexOf(c.url) == 0) {
+        c.postMessage(text);
+      }
+    });
+  });
+}
+
+
+var memoryCachedAMPs = new Map();
 
 self.addEventListener("fetch", function(e) {
+  // NOTE: we don't have support for e.request.mode == "navigate" yet,
+  //  so this is really ugly
+  var url = new URL(e.request.url);
+  if(url.search.indexOf("nosw=1") >= 0) {
+    return; // Fall through to the network
+  }
+
+  var AMPOnlyUrl = new URL(e.request.url);
+  AMPOnlyUrl.search = "AMPOnly=1";
+
   if (e.request.url.indexOf(root + "demo/") >= 0) {
+    if(url.href == AMPOnlyUrl.href) {
+      // Look for it in the memory cache
+      if (memoryCachedAMPs.has(AMPOnlyUrl.href)) {
+        // log("We got it!", memoryCachedAMPs.get(AMPOnlyUrl.href));
+        e.respondWith(memoryCachedAMPs.get(AMPOnlyUrl.href).clone());
+        return;
+      }
+
+      // Look for it in the disk cache
+      log("looking for it in the disk cache");
+
+      // As a last resort, fetch it again
+    }
+
     return e.respondWith(caches.open(CORE_CACHE).then(function(cache) {
       // Go to the network for hte latest
       e.waitUntil(fetch(e.request).then(function(response) {
         response.clone().text().then(function(responseText) {
           // Splice out the text of the response and post it over to the main
           // document.
-          var end = responseText.lastIndexOf(END_AMP_CONTENT);
-          responseText = responseText.substring(0, end);
-          var start = responseText.indexOf(START_AMP_CONTENT);
-          responseText = responseText.substring(start +
-                                                (START_AMP_CONTENT).length);
+          responseText = AMPOnly(responseText);
+          // Put it in the memory and disk caches
+          var syntheticResponse = new Response(responseText);
+          memoryCachedAMPs.set(AMPOnlyUrl.href, syntheticResponse.clone());
           // Forward it on via postMessage
-          return clients.matchAll({ type: "window" }).then(function(clients) {
-            clients.forEach(function(c) {
-              if (e.request.url.indexOf(c.url) == 0) {
-                c.postMessage(responseText);
-              }
-            });
-          }, err);
+          return postToMatchingWindow(responseText, e.request.url).then(
+            function() {
+              // Write to the disk cache
+              caches.open(ITEM_CACHE).then(function(cache) {
+                return cache.put(AMPOnlyUrl.href,  syntheticResponse.clone());
+              });
+            },
+            err
+          );
         }, err);
       }, err));
       // Meanwhile, get the party started by sending back the app shell
